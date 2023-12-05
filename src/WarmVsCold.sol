764b85334bd942756a392d0537810632aba709ea
Simon Tan <simon.tan.yu.jing@gmail.com>
2023-12-05 17:58:12 +0800

    added source code for warm vs cold access

---
+++ src/WarmVsCold.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.20;
+
+contract ColdAccess {
+    uint256 x = 1;
+
+    function getX() public view returns (uint256 a) {
+        a = x;
+    }
+}
+
+contract ColdAndWarmAccess {
+    uint256 x = 1;
+
+    function getX() public view returns (uint256 a) {
+        a = x;
+        a = a + x;
+    }
+}
