af5b75a51712930806d321b55ce1274676cf6122
Simon Tan <simon.tan.yu.jing@gmail.com>
2023-12-10 18:06:28 +0800

    added code for SSTORE

---
+++ src/SstoreCost.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+contract ZeroToNonZero {
+    uint256 public x;
+
+    // Costs 20k + 2.1k gas
+    function setX() public {
+        x = 1;
+    }
+}
+
+contract NonZeroToDiffNonZero {
+    uint256 public x = 1;
+
+    // Costs 2.9k + 2.1k gas
+    function setX() public {
+        x = x + 1;
+    }
+}
+
+contract NonZeroToSameNonZero {
+    uint256 public x = 1;
+
+    // Costs 100 + 2.1k gas
+    function setX() public {
+        // You need to do this otherwise the compiler
+        // will optimize and remove the SSTORE
+        x = x + 1 - 1;
+    }
+}
+
+contract MultipleSstores {
+    uint256 public x = 1;
+
+    // Costs 2.9k (first SSTORE) + 2.1k (first SLOAD) +
+    // 100 (second SSTORE) + 100 (second SLOAD) +
+    // 100 (third SSTORE) + 100 (third SLOAD)
+    function setX() public {
+        x += 3;
+        x -= 1;
+        x -= 1;
+    }
+
+}
---
+++ test/SstoreCost.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {Test} from "forge-std/Test.sol";
+import {
+    ZeroToNonZero,
+    NonZeroToDiffNonZero,
+    NonZeroToSameNonZero,
+    MultipleSstores
+} from "../src/SstoreCost.sol";
+
+// forge test --match-contract SstoreCostTest --gas-report
+contract SstoreCostTest is Test {
+    ZeroToNonZero public zeroToNonZero;
+    NonZeroToDiffNonZero public nonZeroToDiffNonZero;
+    NonZeroToSameNonZero public nonZeroToSameNonZero;
+    MultipleSstores public multipleSstores;
+
+    function setUp() public {
+        zeroToNonZero = new ZeroToNonZero();
+        nonZeroToDiffNonZero = new NonZeroToDiffNonZero();
+        nonZeroToSameNonZero = new NonZeroToSameNonZero();
+        multipleSstores = new MultipleSstores();
+    }
+
+    function test_zeroToNonZero() public {
+        zeroToNonZero.setX();
+    }
+
+    function test_nonZeroToDiffNonZero() public {
+        nonZeroToDiffNonZero.setX();
+    }
+
+    function test_nonZeroToSameNonZero() public {
+        nonZeroToSameNonZero.setX();
+    }
+
+    function test_multipleSstores() public {
+        multipleSstores.setX();
+    }
+}
