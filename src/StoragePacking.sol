2fd04b86ec49934b779a42af87a32156501ebf7c
Simon Tan <simon.tan.yu.jing@gmail.com>
2023-12-14 16:34:16 +0800

    Added storage packing code, tests

---
+++ src/StoragePacking.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.8.22;
+
+contract StoragePacking {
+    // Naive implementation
+    // mapping from student id => grade
+    mapping(uint256 => uint256) gradeForA;
+    mapping(uint256 => uint256) gradeForB;
+    mapping(uint256 => uint256) gradeForC;
+    mapping(uint256 => uint256) gradeForD;
+
+    function recordGradesUnoptimized(uint256 id, uint256 a, uint256 b, uint256 c, uint256 d) public {
+        gradeForA[id] = a;
+        gradeForB[id] = b;
+        gradeForC[id] = c;
+        gradeForD[id] = d;
+    }
+
+    function getGradesUnoptimized(uint256 id) public view returns(uint256, uint256, uint256, uint256) {
+        return (gradeForA[id], gradeForB[id], gradeForC[id], gradeForD[id]);
+    }
+
+    // Optimized implementation
+    // mapping from student id => packed grades
+    mapping(uint256 => uint256) grades;
+
+    function recordGradesOptimized(uint256 id, uint256 a, uint256 b, uint256 c, uint256 d) public {
+        uint256 packedGrades = (((((a << 64) | b) << 64) | c) << 64) | d;
+        grades[id] = packedGrades;
+    }
+
+    function getGradesOptimized(uint256 id) public view returns(uint256 a, uint256 b, uint256 c, uint256 d) {
+        uint256 packedGrades = grades[id];
+        a = packedGrades >> 192;
+        b = packedGrades >> 128 & type(uint64).max;
+        c = packedGrades >> 64 & type(uint64).max;
+        d = uint256(uint64(packedGrades));
+    }
+}
---
+++ test/StoragePacking.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {Test} from "forge-std/Test.sol";
+import {StoragePacking} from "../src/StoragePacking.sol";
+
+// forge test --match-contract StoragePackingTest --gas-report
+contract StoragePackingTest is Test {
+    StoragePacking public storagePacking;
+
+    function setUp() public {
+        storagePacking = new StoragePacking();
+        storagePacking.recordGradesUnoptimized(1, 66, 77, 88, 99);
+        storagePacking.recordGradesOptimized(1, 66, 77, 88, 99);
+    }
+
+    function test_recordGradesUnoptimized(uint256 id, uint256 a, uint256 b, uint256 c, uint256 d) public {
+        vm.assume(id != 1);
+        storagePacking.recordGradesUnoptimized(id, a, b, c, d);
+    }
+
+    function test_getGradesUnoptimized() public {
+        storagePacking.getGradesUnoptimized(1);
+    }
+
+    function test_recordGradesOptimized(uint256 id, uint256 a, uint256 b, uint256 c, uint256 d) public {
+        vm.assume(id != 1);
+        storagePacking.recordGradesOptimized(id, a, b, c, d);
+    }
+
+    function test_getGradesOptimized() public {
+        storagePacking.getGradesOptimized(1);
+    }
+}
